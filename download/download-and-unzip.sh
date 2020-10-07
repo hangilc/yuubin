a5ee3a4c51c6e0cb584f14df297df184fc4b4b52
Hangil Chang <hangil@chang.jp>
2020-10-07 18:01:00 +0900

    reorganized directory structure

--- download/download-and-unzip.sh
+++ download/download-and-unzip.sh
@@ -2,5 +2,7 @@ cd /data
 curl -L https://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip >ken_all.zip
 rm -f KEN_ALL.CSV
 unzip ken_all.zip
-chmod 666 KEN_ALL.CSV
+iconv -f SHIFT_JIS KEN_ALL.CSV -t utf-8 -o jp-postalcodes.csv
+chmod 666 jp-postalcodes.csv
+rm KEN_ALL.CSV
 rm ken_all.zip
